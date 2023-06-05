3eabb6b32b6acbfbbc52cd8634fd56f48d49c5de
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-06-05 13:39:21 +0530

    routes files

--- routes/shop.js
+++ routes/shop.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const shopController = require('../controllers/shop');
+const { scheduler } = require('timers/promises');
 
 const router = express.Router();
 
@@ -13,6 +14,10 @@ router.get('/products', shopController.getProducts);
 router.get('/products/:productId',shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
+
+router.post('/cart',shopController.postCart)
+
+router.post('/cart-delete-item',shopController.postCartDeleteProduct);
 
 router.get('/orders', shopController.getOrders);
 
