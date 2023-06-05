ca08cb8f44edf8b504457ac1a187620b91cca962
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-06-05 19:18:07 +0530

    Main server file after sequelize

--- cleanserver.js
+++ cleanserver.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -12,14 +12,8 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { resourceUsage } = require('process');
 
-db.execute('SELECT * FROM products')
-.then(result=>{
-    console.log(result[0],result[1]);
-})
-.catch(err=>{
-    console.log(err);
-});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -29,5 +23,13 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync()
+.then(result=>{
+    // console.log(result);
+    app.listen(3000);
+})
+.catch(err=>{
+    console.log(err);
+})
+
 
